b9005f55a716538c1ac549158365bf0de81efdff
rajguptaaa <kumaaraj11@gmail.com>
2025-12-10 21:56:29 +0530

    deployment changes

--- server/server.js
+++ server/server.js
@@ -14,11 +14,17 @@ dotenv.config()
 const app = express()
 
 app.use(cors({
-  origin: ['http://localhost:3000', 'http://localhost:5173'],
+  origin: [
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'https://hrmanagementhub.vercel.app'
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
 }))
+
+
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
